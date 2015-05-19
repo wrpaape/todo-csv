e2e4d4980ae6a0efb6e93ee87fd26efed650dd0b
Reid Paape <wrpaape@gmail.com>
2015-05-19 16:52:25 -0500

    added edit_todo method

--- lib/todo.rb
+++ lib/todo.rb
@@ -18,13 +18,15 @@ class Todo
 
       puts
       puts "What would you like to do?"
-      puts "1) Exit 2) Add Todo 3) Mark Todo As Complete"
+      puts "1) Exit 2) Add Todo 3) Mark Todo As Complete 4) Edit a Todo 5) Delete a Todo"
       print " > "
       action = gets.chomp.to_i
       case action
       when 1 then exit
       when 2 then add_todo
       when 3 then mark_todo
+      when 4 then edit_todo
+      when 5 then delete_todo
       else
         puts "\a"
         puts "Not a valid choice"
@@ -38,9 +40,9 @@ class Todo
 
   def view_todos
     puts "Unfinished"
-    puts unfinished
+    unfinished.each{ |row| puts row}
     puts "Completed"
-    puts completed
+    completed.each{ |row| puts row}
   end
 
   def unfinished
@@ -72,6 +74,42 @@ class Todo
         end
       end
     end
+
+    save!
+  end
+
+  def edit_todo
+    puts "Is the todo you want to edit finished (y/n)?"
+    is_completed = get_input.downcase
+    if is_completed == "y"
+      completed_value = "yes"
+      puts "Which completed todo would you like to edit?"
+      todo_index = get_input.to_i
+    elsif is_completed == "n"
+      completed_value = "no"
+      puts "Which unfinished todo would you like to edit?"
+      todo_index = get_input.to_i
+    else
+      return
+    end
+    puts "new name for todo: "
+    new_todo = get_input
+
+    edited_index = 0
+    @todos.each_with_index do |row, index|
+      if row["completed"] == completed_value
+        edited_index += 1
+        if edited_index == todo_index
+          @todos[index]["name"] = new_todo
+        end
+      end
+    end
+
+  save!
+  end
+
+  def delete_todo
+
   end
 
   private
