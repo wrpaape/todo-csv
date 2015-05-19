a0da8d436ac6c31f8591dbf68f0c0e6426141c34
Justin Herrick <justin@theironyard.com>
2015-05-19 00:30:21 -0500

    moving test setup to spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+module Kernel
+  # For testing purposes. Never do this
+  def print(msg)
+    $output << msg
+  end
+  def puts(msg = '')
+    $output << msg
+  end
+  # Oh god never do this
+  def gets
+    Struct.new(:chomp).new
+  end
+end
+
+# Overriding Todo methods for easier testing
+class Todo
+  def get_input
+    ($input ||= []).last #Use our global input instead of asking for actual user input
+  end
+  def save!
+    nil # Don't change our test file
+  end
+end
+# Dont touch ^^^
+
+
--- spec/todo_test.rb
+++ spec/todo_test.rb
@@ -1,31 +1,6 @@
 require 'test/unit'
 require_relative '../lib/todo'
-
-
-module Kernel
-  # For testing purposes. Never do this
-  def print(msg)
-    $output << msg
-  end
-  def puts(msg = '')
-    $output << msg
-  end
-  # Oh god never do this
-  def gets
-    Struct.new(:chomp).new
-  end
-end
-
-# Overriding Todo methods for easier testing
-class Todo
-  def get_input
-    ($input ||= []).last #Use our global input instead of asking for actual user input
-  end
-  def save!
-    nil # Don't change our test file
-  end
-end
-# Dont touch ^^^
+require_relative './spec_helper'
 
 class TodoTest < Test::Unit::TestCase
 
